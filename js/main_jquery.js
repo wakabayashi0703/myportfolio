2b9864857b89592118b2c5f6b6f88d1ee6921451
Mitsuo Wakabayashi from linuxPC <ufc320wwe@gmail.com>
2020-10-29 00:08:25 +0900

    animation complite

--- js/main_jquery.js
+++ js/main_jquery.js
@@ -105,14 +105,280 @@ $(function () {
 
         });
 
+        // $('.seek_line').addClass('.reset-bar');
+        // $('.seek_line').addClass('start-bar');
+        
+
+        // $('.seek_line').addClass('start-slide');
+
+        // var startbar = $('.seek_line').animate({'left':'0%'},3000);
+        // var endbar = $('.seek_line').animate({'left':'100%'},700);
+
+
+        // $('.start-slide').on('animationend webkitAnimationEnd',function(){
+        //     // CSSのanimationプロパティで設定したものが終了した時に実行する内容 
+        // // $('.seek_line').css('backgroundColor','red');
+        // $('.start-slide').css('backgroundColor','red');
+        // // $('.seek_line').addClass('end-bar');
+
+        // $('.start-slide').removeClass('start-slide');
+        // $('.seek_line').addClass('end-slide');
+
+        // $('.start-slide').removeClass('.start-slide');
+
+    
+        // });
+
+        // $('.end-slide').on('animationend webkitAnimationEnd',function(){
+        //     // CSSのanimationプロパティで設定したものが終了した時に実行する内容 
+
+        // $('.seek_line').addClass('start-slide');
+        // $('.end-slide').removeClass('end-slide');
+
+
+        // $('.start-slide').removeClass('.start-slide');
+
+    
+        // });
+
+        //試供品test02
+        // $('.seek_line').addClass('start-bar');
+        // gspa.to('.seek_line',{ left: 210 });
+        // gsap.to(".seek_line", {
+        //     delay:0.7,
+        //     duration:3,
+        //     x: 210,
+        //     onComplete:
+        //     function(){
+        //         gsap.to(".seek_line",{
+        //             delay:0,
+        //             duration:0.7,
+        //             x:+420,
+        //             onComplete:
+        //             function(){
+        //                 gsap.to(".seek_line",{
+        //                     delay:0,
+        //                     duration:0,
+        //                     x:0,
+
+        //                 })
+                        
+        //             },
+        //         });
+
+        //     },
+        // });
+
+        // gsap.to(".seek_line", {
+        //     delay:0.7,
+        //     duration:3,
+        //     x: 210
+        // });
+
+
+        //試供品test02
+
+
+        // gsap.to(".seek_line",{
+        //     delay:0,
+        //     duration:0.7,
+        //     x:420,
+        //     onComplete:
+        //     function(){
+        //         gsap.to(".seek_line",{
+        //             delay:0,
+        //             duration:0,
+        //             x:0,
+
+        //             onComplete:
+        //             function(){
+        //                 gsap.to(".seek_line",{
+        //                     delay:0,
+        //                     duration:3,
+        //                     x:210,
+        //                 });
+        //             }
+
+        //         })
+                
+        //     },
+        // });
+        gsap.to(".seek_line",{
+            delay:0.7,
+            duration:3,
+            x:210,
+            // ease:"none",
+            overwrite:true,
+        });
+
+          
+
+        // 完成品test01
+        // $('.seek_line').animate({'left':'0%'},3000);
+        // 完成品test01
+
 
         $('.slider_list').on('beforeChange', function (event, slick, curentSlide, nextSlide) {
             $('.sub_sub_slider_list').slick('slickGoTo', nextSlide, false);
-            
-            //seek-line をleft 100% へ　700ms で
-            $('.seek-line').animate({
-                'left':'100%'
+
+            // 完成品test01
+            // $('.seek_line').stop().animate({'left':'100%'},700)
+            // // $('.seek_line').stop().css('left','50%');
+            // .animate({'left':'-100%'},0)
+            // // $('.seek_line').animate({'left':'0%'},3000);
+            // .animate({'left':'0%'},3000);
+            // 以上正常な動きが完成
+            // 完成品test01
+
+
+
+            //試供品test02
+            // $('.seek_line').stop();
+
+                    
+
+
+
+            gsap.to(".seek_line",{
+                delay:0,
+                duration:0.7,
+                x:420,
+                overwrite:true,
+                onComplete:
+                function(){
+                    gsap.to(".seek_line",{
+                        delay:0,
+                        duration:0,
+                        x:0,
+                        overwrite:true,
+    
+                        onComplete:
+                        function(){
+                            gsap.to(".seek_line",{
+                                delay:0,
+                                duration:3,
+                                x:210,
+                                // ease:"none",
+                                overwrite:true,
+                            });
+                        }
+    
+                    })
+                    
+                },
             });
+
+
+
+
+
+
+
+            // gsap.to(".seek_line", {
+            //     delay:0.7,
+            //     duration:3,
+            //     x: 210,
+            //     onComplete:
+            //     function(){
+            //         gsap.to(".seek_line",{
+            //             delay:0,
+            //             duration:0.7,
+            //             x:+420,
+            //             onComplete:
+            //             function(){
+            //                 gsap.to(".seek_line",{
+            //                     delay:0,
+            //                     duration:0,
+            //                     x:0,
+    
+            //                 })
+                            
+            //             },
+            //         });
+    
+            //     },
+            // });
+            
+
+            //試供品test02
+
+
+
+
+
+
+
+            // $('.seek_line').animate({'left':'0%'},3000)
+            // .animate({'left':'100%'},700);
+        // $('.seek_line').removeClass('start-bar');
+        // $('.seek_line').addClass('end-bar');
+        // $('.seek_line').removeClass('end-bar');
+
+        // $('.seek_line').addClass('reset-bar');
+        // $('.seek_line').removeClass('reset-bar');
+        // $('.seek_line').addClass('start-bar');
+
+
+
+
+        // $('.seek_line').addClass('.reset-bar');
+
+
+            // $('.seek_line')
+
+            // var $progressAS = $$(prefix + "nav__control-progress", $slider);
+            // $prevControl.classList.add("m--reset-progress");
+            // var triggerLayout = $prevControl.offsetTop;
+            // $prevControl.classList.remove("m--reset-progress");
+
+            // function setAutoslidingTO() {
+            //     window.clearTimeout(autoSlidingTO);
+            //     var delay = +options.autoSlidingDelay || autoSlidingDelay;
+            //     curSlide++;
+            //     if (curSlide > numOfSlides) curSlide = 1;
+          
+            //     autoSlidingTO = setTimeout(function() {
+            //       performSliding(curSlide);
+            //     }, delay);
+            //   };
+          
+            //   if (options.autoSliding || +options.autoSlidingDelay > 0) {
+            //     if (options.autoSliding === false) return;
+                
+            //     autoSlidingActive = true;
+            //     setAutoslidingTO();
+                
+            //     $slider.classList.add("m--with-autosliding");
+            //     var triggerLayout = $slider.offsetTop;
+                
+            //     var delay = +options.autoSlidingDelay || autoSlidingDelay;
+            //     delay += slidingDelay + slidingAT;
+                
+            //     $progressAS.forEach(function($progress) {
+            //       $progress.style.transition = "transform " + (delay / 1000) + "s";
+            //     });
+            //   }
+              
+            //   $slider.querySelector(".fnc-nav__control:first-child").classList.add("m--active-control");
+          
+            // };
+            // .animate({'left':'100%'},700)
+            // .animate('left','-100%',0)
+            // .animate({'left':'0%'},3000);
+            //seek-line をleft 100% へ　700ms で
+            // $('.seek-line').css('left','100%');
+            // $('.seek_line').addClass('.left-100');
+            // $('.seek_line').addClass('.end-slide');
+            // $('.seek_line').addClass('start-slide');
+
+
+            // $('.seek-line').animate({'left':'100%'},
+            // {
+            //     'duration': 600,
+            //     'complete': function(){
+            //         // alert('終わりました');
+            //     }
+            //     });
 
 
             //seek-line を left -100% へ
